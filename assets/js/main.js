6e80d42cf9511a59c73a0a5e2bb07df2d05d52e7
Cristhian Camilo Suarez <camilo.svanegas@gmail.com>
2022-05-04 12:44:27 -0500

    Avances proyecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,34 @@
 //Get JSON of portfolio works (async)
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
+var requestWorks = new XMLHttpRequest();
+requestWorks.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        let parsedJson = JSON.parse(xhttp.responseText);
-        //List of categories        
-        var item = '<ul>'
-        parsedJson.forEach(type => {
-            item += '<li '+'id="'+type.category+'">'+type.category+'</li>';
+        let parsedWorks = JSON.parse(requestWorks.responseText);
+        //Create  list of all works
+        var allWorks = '<ul>';
+        parsedWorks.forEach(work => {
+            allWorks += '<li style="background-image: url('+work.image+');">'+work.title+'</li>';
+        });
+        allWorks += '</ul>';
+        document.getElementById('works-grid').innerHTML = allWorks;
+    }
+};
+requestWorks.open("GET", "portfolio-works.json", true);
+requestWorks.send();
+//Get JSON of portfolio categories (async)
+var requestCategories = new XMLHttpRequest();
+requestCategories.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let parsedCategories = JSON.parse(requestCategories.responseText);
+        //List of categories (tab control)        
+        var item = '<ul>';
+        parsedCategories.forEach(category => {
+            item += '<li '+'id="'+category.title+'">'+category.title+'</li>';
         });
         item += '</ul>'; 
         document.getElementById('categories-control').innerHTML = item;
-        //Grid per category
-        var newGrid = '';
-        parsedJson.forEach(father => {
-            const children = [];
-            const getChildren = father.works;
-            children.push(getChildren);
-            newGrid += '<ul '+'id="grid-'+father.category+'">'+children+'</ul>'
-            console.log(children);
-        });
-        newGrid = newGrid;
-        document.getElementById('categories-grids').innerHTML = newGrid;
     }
 };
-xhttp.open("GET", "portfolio-works.json", true);
-xhttp.send();
+requestCategories.open("GET", "portfolio-categories.json", true);
+requestCategories.send();
+//
 
